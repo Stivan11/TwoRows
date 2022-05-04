eccb0217600728fc969aa332796815df3fa04904
Stivan11 <104930798+Stivan11@users.noreply.github.com>
2022-05-04 21:24:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TwoRows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine()
+                .Split(',')
+                .Select(int.Parse)
+                .ToList();
+
+            var second = Console.ReadLine()
+                .Split(',')
+                .Select(int.Parse)
+                .ToList();
+
+            var sorted = new List<int>(first);
+
+            for (int i = 0; i < second.Count; i++)
+            {
+                sorted.Add(second[i]);
+            }
+
+            sorted.Sort();
+
+            var queue = new Queue<int>(sorted);
+
+            Console.WriteLine(string.Join(", ", sorted));
+
+            
+        }
+    }
+}
